0dcafece737ea7f9c8009268bd8675adc639375c
NguyenVanAn-200704 <nva2004gl81@gmail.com>
2024-10-23 19:07:54 +0700

    push An

--- Job/Job/Program.cs
+++ Job/Job/Program.cs
@@ -16,7 +16,7 @@ namespace Job
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FDangNhap());
+            Application.Run(new FNguoiUngTuyen());
         }
     }
 }
